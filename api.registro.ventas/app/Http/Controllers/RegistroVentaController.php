c9b4a034a6b8412daf461994be9b7903354caca4
Marcelo Rivero <rivero_marcelo@hotmail.com>
2022-11-12 21:20:28 -0300

    Cambios en api.registroVentas

--- api.registro.ventas/app/Http/Controllers/RegistroVentaController.php
+++ api.registro.ventas/app/Http/Controllers/RegistroVentaController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\RegistroVenta;
 use Illuminate\Http\Request;
 
 class RegistroVentaController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+
+        $venta = RegistroVenta::create([
+
+            'idCliente' => $request->idCliente,
+            'idProducto' => $request->idProducto,
+            
+        ]);
+
+        return $venta;
+
+
+    }
 }
